9944e65113c0d615f25558ff66faa490d95c3f00
jsanson1747 <jsanson1747@gmail.com>
2024-03-21 13:53:40 -0400

    Styled the Login Screen

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,15 +1,25 @@
 import AuthForm from "@/components/forms/AuthForm";
 import { auth } from "../auth";
 import Link from "next/link";
+import { Box, Stack } from "@mui/material";
 
 export default async function LoginPage() {
   const session = await auth();
 
   return (
-    <div>
+    <Stack
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100%",
+        paddingTop: 22,
+      }}
+    >
       <AuthForm session={session} />
-      <br />
-      {!session && <Link href="/auth/signup">New? Signup here!</Link>}
-    </div>
+      <Box sx={{ display: "flex", justifyContent: "center", marginTop: 1 }}>
+        {!session && <Link href="/auth/signup">New? Signup here!</Link>}
+      </Box>
+    </Stack>
   );
 }
--- components/forms/AuthForm.tsx
+++ components/forms/AuthForm.tsx
@@ -1,5 +1,14 @@
 "use client";
 
+import {
+  Box,
+  Button,
+  Card,
+  Input,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { Session } from "next-auth";
 import { signIn, signOut } from "next-auth/react";
 import Link from "next/link";
@@ -34,32 +43,44 @@ export default function AuthForm({ session }: Props) {
   };
 
   return (
-    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
-    <div>
-      {!session && (
-        <form onSubmit={handleSubmit(onSubmit)}>
-          Username
-          <br />
-          <input {...register("username", { required: true })} />
-          {errors.username && <span>This field is required</span>}
-          <br />
-          Password
-          <br />
-          <input
-            {...register("password", { required: true })}
-            type="password"
-          />
-          {errors.password && <span>This field is required</span>}
-          <br />
-          <input type="submit" />
-        </form>
-      )}
-      {session && (
-        <div>
-          <Link href="/">Back</Link>
-          <button onClick={handleSignOut}>Sign out</button>
-        </div>
-      )}
-    </div>
+    <Card>
+      <Box sx={{ padding: 3 }}>
+        {!session && (
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Typography color="text.secondary" textAlign="center" variant="h4">
+              Log In
+            </Typography>
+            <Stack paddingTop={2.8}>
+              <TextField
+                label="Username"
+                {...register("username", { required: true })}
+                error={Boolean(errors.username)}
+                helperText={errors.username ? "This field is required" : null}
+                sx={{ marginBottom: errors.username ? 0 : 2.8 }}
+              />
+
+              <TextField
+                label="Password"
+                {...register("password", { required: true })}
+                type="password"
+                error={Boolean(errors.password)}
+                helperText={errors.password ? "This field is required" : null}
+                sx={{ marginBottom: errors.password ? 0 : 2.8 }}
+              />
+
+              <Button type="submit" variant="outlined">
+                Submit
+              </Button>
+            </Stack>
+          </form>
+        )}
+        {session && (
+          <Stack>
+            <Link href="/">Back</Link>
+            <button onClick={handleSignOut}>Sign out</button>
+          </Stack>
+        )}
+      </Box>
+    </Card>
   );
 }
