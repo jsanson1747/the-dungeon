260997aae4d151741b6a2235556b64a50375eef7
jsanson1747 <jsanson1747@gmail.com>
2024-03-24 01:47:46 -0400

    Remove users api route

--- app/api/users/route.ts
+++
@@ -1,7 +0,0 @@
-import supabase from "@/supabase/client";
-
-export async function GET() {
-  const { data, error } = await supabase.from("users").select("*");
-
-  return Response.json({ data: data });
-}
